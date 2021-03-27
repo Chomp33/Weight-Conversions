c74c23c51eddb4d4571984667a2af2d5e10d4841
Greg Macionsky <81204016+Chomp33@users.noreply.github.com>
2021-03-27 16:33:46 -0400

    Add files via upload

---
+++ Lbs to KGs.py
@@ -0,0 +1,12 @@
+# This program will convert pounds into kilograms
+
+print('How many pounds does the object weigh?')
+
+# User inputs weight in pounds and program converts to an integer
+# lbs = variable (pounds)
+
+lbs = int(input())
+
+print('The object weighs:')
+
+print(lbs / 2.2, 'kilograms')
